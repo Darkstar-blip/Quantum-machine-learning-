bb8212e3740882170f537d98961219df96d40144
Darkstar-blip <dwivediaakash37@gmail.com>
2025-11-08 10:06:47 +0000

    chore: remove temporary helper scripts

--- qml-benchmark/inspect_qml_kernels.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import qiskit_machine_learning as qml
-import qiskit_machine_learning.kernels as km
-import inspect, sys, os
-print('qml package version:', getattr(qml, '__version__', 'no-version'))
-print('kernels module file:', km.__file__)
-print('dir(kernels):', dir(km))
-print('\n--- kernels __init__ content ---\n')
-print(open(km.__file__, 'r').read())
